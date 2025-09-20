87a397de5aed2135bdd03a90c7a900662d608e42
Marios Tsigkas <mariosh346@gmail.com>
2025-09-20 18:50:57 +0300

    feat(routing): implement background-location overlay pattern for modal deep links

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,58 @@
-import { Routes, Route, Link } from 'react-router-dom'
-import { lazy, Suspense } from 'react'
-import './App.css'
+import React, { lazy, Suspense } from "react";
+import { Routes, Route, useLocation, Link } from "react-router-dom";
+import "./App.css";
 
-const loadHome = () => import('./views/Home')
-const loadImageView = () => import('./views/ImageView')
-const loadFavorites = () => import('./views/Favorites')
-const loadBreeds = () => import('./views/Breeds')
-const loadBreedDetail = () => import('./views/BreedDetail')
+const loadHome = () => import("./views/Home");
+const loadImageView = () => import("./views/ImageView"); // This will be CatModal
+const loadFavorites = () => import("./views/Favorites");
+const loadBreeds = () => import("./views/Breeds");
+const loadBreedDetail = () => import("./views/BreedDetail"); // This will be BreedModal
 
-const Home = lazy(loadHome)
-const ImageView = lazy(loadImageView)
-const Favorites = lazy(loadFavorites)
-const Breeds = lazy(loadBreeds)
-const BreedDetail = lazy(loadBreedDetail)
+const Home = lazy(loadHome);
+const CatModal = lazy(loadImageView); // Renamed ImageView to CatModal for clarity
+const Favorites = lazy(loadFavorites);
+const Breeds = lazy(loadBreeds);
+const BreedModal = lazy(loadBreedDetail); // Renamed BreedDetail to BreedModal for clarity
 
-function App() {
+const App: React.FC = () => {
+  const location = useLocation();
+  const state = location.state as { backgroundLocation?: Location } | undefined;
+
   return (
     <div>
       <nav>
         <Link to="/" onMouseEnter={() => void loadHome()}>
           Home
         </Link>
-        <span className='px-1'>|</span>
+        <span className="px-1">|</span>
         <Link to="/breeds" onMouseEnter={() => void loadBreeds()}>
           Breeds
         </Link>
-        <span className='px-1'>|</span>
+        <span className="px-1">|</span>
         <Link to="/favorites" onMouseEnter={() => void loadFavorites()}>
           Favorites
         </Link>
       </nav>
-      
+
       <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
+        {/* Render main UI using backgroundLocation (if present) so it stays behind modal */}
+        <Routes location={state?.backgroundLocation ?? location}>
           <Route path="/" element={<Home />} />
-          <Route path="/image/:imageId" element={<ImageView />} />
           <Route path="/breeds" element={<Breeds />} />
-          <Route path="/breed-detail/:breedId" element={<BreedDetail />} />
           <Route path="/favorites" element={<Favorites />} />
+          {/* Add other non-modal routes here */}
         </Routes>
+
+        {/* If we have a background location, render modal routes as overlays */}
+        {state?.backgroundLocation && (
+          <Routes>
+            <Route path="/image/:id" element={<CatModal />} />
+            <Route path="/breed/:id" element={<BreedModal />} />
+          </Routes>
+        )}
       </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
